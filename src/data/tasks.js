da9bcbc1875500a8b1cde1a26c832a23c6d19cd2
Boris Quezada <bquezada1988@gmail.com>
2024-02-28 22:46:03 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "My first task",
+    description: "my first task",
+  },
+  {
+    id: 1,
+    title: "My second task",
+    description: "my second task",
+  },
+  {
+    id: 2,
+    title: "My third task",
+    description: "my third task",
+  },
+];
